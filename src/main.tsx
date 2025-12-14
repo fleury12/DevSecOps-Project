a2292b7f75a5d93d5dbff90e4d968c2584cfcad9
angekoffi160 <angekoffi160@gmail.com>
2025-12-14 20:11:45 +0100

    add apm agent

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ import { extendedApi } from "./store/slices/configuration";
 import palette from "./theme/palette";
 import router from "./routes";
 import MainLoadingScreen from "./components/MainLoadingScreen";
-
+import { init as initApm } from '@elastic/apm-rum';
 store.dispatch(extendedApi.endpoints.getConfiguration.initiate(undefined));
 
 const root = ReactDOM.createRoot(
@@ -30,3 +30,10 @@ root.render(
     </React.StrictMode>
   </Provider>
 );
+
+
+initApm({
+  serviceName: 'devsecops-frontend',
+  serverUrl: 'http://localhost:8200', // via port-forward
+  environment: 'development',
+});
